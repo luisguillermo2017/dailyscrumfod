9cc7309b1f2ef8d16f37803a9a0b955d73d1e2b1
Luis Loaiza <luisguillermo2017@gmail.com>
2022-05-01 18:22:43 -0600

    Primer docs

---
+++ docs/precache-manifest.f21391e91e2182d8ae992d82108ee866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de0005903c2e0120b26398142e31155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f96c5953ae3ff574291",
+    "url": "/static/css/main.c11792e3.chunk.css"
+  },
+  {
+    "revision": "f1ceeae124cc2f12f13b",
+    "url": "/static/js/2.7cc8e56a.chunk.js"
+  },
+  {
+    "revision": "0f96c5953ae3ff574291",
+    "url": "/static/js/main.89fc34ef.chunk.js"
+  },
+  {
+    "revision": "69e3d355583ba21c2f76",
+    "url": "/static/js/runtime~main.c83a2fcd.js"
+  }
+]);
